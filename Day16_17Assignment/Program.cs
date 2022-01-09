b63d52fba5daed2f872f0a5ebde57db5451fc85d
hkvijay54 <hkvijay54@gmail.com>
2022-01-10 00:31:43 +0530

    uc4

--- Day16_17Assignment/Program.cs
+++ Day16_17Assignment/Program.cs
@@ -1,20 +1,39 @@
-﻿using Binary_Search_Word;
-using System;
+﻿using System;
 
-namespace BinarySearchTree
-{    
+namespace InsertionSort
+{
     class Program
-    {
+    {    
         static void Main(string[] args)
         {
-            BinaryTree binaryTree = new BinaryTree();
-
-            binaryTree.Add(1);
-            binaryTree.Add(2);
-            binaryTree.Add(7);
-            binaryTree.Add(3);
-            binaryTree.Add(0);
-            binaryTree.TraversePreorder(binaryTree.Root);
+            int[] arr = new int[5] { 23, 9, 85, 12, 99 };
+            int n = 5, i, j, val, flag;
+            Console.WriteLine("Insertion Sort");
+            Console.WriteLine("Intial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+            Console.Write("\nSorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
         }
     }
 }
