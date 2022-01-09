a4f73ab4032091848cc9b5d200f3ea76d6dba81f
hkvijay54 <hkvijay54@gmail.com>
2022-01-09 23:38:30 +0530

    uc1

--- Day16_17Assignment/Program.cs
+++ Day16_17Assignment/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Datastructure programs");
+﻿using System;
+
+namespace TwoStringAreEqual
+{
+    class Program
+    {
+        public static bool checkEquality(String[] s1, String[] s2)
+        {
+            if (s1 == s2)
+            {
+                return true;
+            }
+            if (s1 == null || s2 == null)
+            {
+                return false;
+            }
+            int n = s1.Length;
+            if (n != s2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (!s1[i].Equals(s2[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        class program
+        {
+            static void Main(String[] args)
+            {
+                String[] s1 = { "A", "B", "C" };
+                String[] s2 = { "A", "B", "C" };
+                if (checkEquality(s1, s2))
+                {
+                    Console.WriteLine("Both arrays are equal");
+                }
+                else
+                {
+                    Console.WriteLine("Both arrays are not equal");
+                }
+            }
+        }
+    }
+}
